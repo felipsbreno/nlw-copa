63bb8a7c3db15442dde6924cf17d314163ff51a3
Breno Felipe de Bairros <Breno.Bairros@cognyte.com>
2022-11-01 00:16:19 -0300

    add: Initial Project

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+async function bootstrap() {
+  const prisma = new PrismaClient({
+    log: ['query'],
+  });
+
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  fastify.register(cors, {
+    origin: true,
+  });
+
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count();
+    return { count };
+  });
+
+  fastify.listen({ port: 3333, host: '0.0.0.0' }, () => {
+    console.log('Server is running!');
+  });
+}
+
+bootstrap();
