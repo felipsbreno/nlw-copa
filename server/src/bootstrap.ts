ea0a75f6625a4d2132767e78fd94d07584fe505f
Breno Felipe de Bairros <Breno.Bairros@cognyte.com>
2022-11-02 15:16:16 -0300

    create new routes to count and register a new pool

--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -1,5 +1,7 @@
 import Fastify from 'fastify';
+import { z as zObject } from 'zod';
 import cors from '@fastify/cors';
+import ShortUniqueId from 'short-unique-id';
 import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
@@ -20,9 +22,33 @@ async function bootstrap() {
     return { count };
   });
 
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count();
+    return { count };
+  });
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+
   fastify.post('/pools', async (request, replay) => {
-    const { title } = request.body;
-    return { title };
+    const createPoolBody = zObject.object({
+      title: zObject.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return replay.status(201).send({ code });
   });
 
   fastify.listen({ port: 3333, host: '0.0.0.0' }, () => {
