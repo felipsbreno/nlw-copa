8c2e8ca55523e8088d1999c449a0ad2c302fb38c
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-02 23:29:52 -0300

    restore color

--- mobile/src/screens/SingIn.tsx
+++ mobile/src/screens/SingIn.tsx
@@ -3,7 +3,7 @@ import { Center, Text } from 'native-base';
 export function SingIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text fontSize={40} color="red.300" fontFamily="heading">
+      <Text fontSize={40} color="yellow.500" fontFamily="heading">
         SingIn
       </Text>
     </Center>
