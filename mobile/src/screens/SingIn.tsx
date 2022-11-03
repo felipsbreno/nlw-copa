a672f5614904a3509d2883f52813f78f1b4cad0c
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-03 00:27:18 -0300

    create the screen of singin

--- mobile/src/screens/SingIn.tsx
+++ mobile/src/screens/SingIn.tsx
@@ -1,10 +1,23 @@
-import { Center, Text } from 'native-base';
+import { Center, Text, Icon } from 'native-base';
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
+import { Fontisto } from '@expo/vector-icons';
 
 export function SingIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text fontSize={40} color="yellow.500" fontFamily="heading">
-        SingIn
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+
+      <Button
+        type="SECONDARY"
+        title="Entrar com a conta do Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   );
